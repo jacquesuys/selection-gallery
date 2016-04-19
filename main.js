ec2be2e6bba88535ec284c4b679338d9e2d8db13
Jacques Uys <jacquesuys@Jacquess-MacBook-Air.local>
2016-04-19 17:32:37 +0200

    Refactor for new HTML structure

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ document.addEventListener("DOMContentLoaded", function(){
 		return document.querySelectorAll(element);
 	};
 
-	var $divs = $('div');
+	var $divs = $('section > div');
 
 	for (var i = 0; i < $divs.length; i++) {
 		$divs[i].dataset.i = i;
+
+		var $cover = $divs[i].querySelector('.cover');
+		var $img = $cover.querySelector('img');
+
+		$cover.style.backgroundImage = 'url(' + $img.src + ')';
+
 		$divs[i].addEventListener('click', function(){
 			var $i = this.dataset.i;
 			if ( selected.length < 10 ) {
@@ -19,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function(){
 					selected.splice($i, 1);
 					console.log(selected);
 				}
-				$divs[$i].classList.toggle("selected");
+				$divs[$i].classList.toggle('selected');
 			}
 		});
 	}
