529b2cb2e0f5fcd6327b9455a9860048ac03f516
Jacques Uys <jacquesuys@Jacquess-MacBook-Air.local>
2016-04-20 19:41:04 +0200

    jQuery familiar syntax

--- main.js
+++ main.js
@@ -1,89 +1,37 @@
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn);
-  } else {
-    document.attachEvent('onreadystatechange', function() {
-      if (document.readyState != 'loading')
-        fn();
-    });
-  }
-}
+$(document).ready(function(){
 
-var main = function(){
+	var $menu = $('header > a');
+	var $header = $('header');
 
-	var $ = function(el) {
-		return document.querySelectorAll(el);
-	};
-
-	var addEventListener = function(el, eventName, handler) {
-	  if (el.addEventListener) {
-	    el.addEventListener(eventName, handler);
-	  } else {
-	    el.attachEvent('on' + eventName, function(){
-	      handler.call(el);
-	    });
-	  }
-	};
-
-	var toggleClass = function(el, className) {
-		if (el.classList) {
-		  el.classList.toggle(className);
-		} else {
-			var classes = el.className.split(' ');
-			var existingIndex = -1;
-			for (var i = classes.length; i--;) {
-				if (classes[i] === className) {
-					existingIndex = i;
-				}
-			}
-
-			if (existingIndex >= 0) {
-				classes.splice(existingIndex, 1);
-			} else {
-				classes.push(className);
-			}
-
-		  el.className = classes.join(' ');
-		}
-	};
-
-	var $menu = $('header > a')[0];
-	var $header = $('header')[0];
-
-	addEventListener($menu, 'click', function(){
-		toggleClass($header, 'menu-open');
+	$menu.on('click', function(){
+		$header.toggleClass('menu-open');
 	});
 
 	var $divs = $('section > div');
 	var selected = [];
 
-	for (var i = 0; i < $divs.length; i++) {
-		$divs[i].dataset.i = i;
+	$divs.each(function(i){
+		$(this).data('i', i);
+		
+		var $cover = $(this).find('.cover');
+		var $src = $cover.find('img').attr('src');
 
-		var $cover = $divs[i].querySelector('.cover');
-		var $img = $cover.querySelector('img');
-
-		$cover.style.backgroundImage = 'url(' + $img.src + ')';
-
-		addEventListener($divs[i], 'click', function(){
-			var $i = this.dataset.i;
+		$cover.css('background-image', 'url(' + $src + ')');
+		$(this).on('click', function(){
+			var $i = $(this).data('i');
 			var $indexOf = selected.indexOf($i);
-
 			if ( selected.length < 10 ) {
 				if ( $indexOf < 0 ) {
 					selected.push($i);
 				} else {
 					selected.splice($indexOf, 1);
 				}
-				toggleClass($divs[$i], 'selected');
+				$(this).toggleClass('selected');
 			} else if (selected.length === 10 && $indexOf > 0) {
 				selected.splice($indexOf, 1);
-				toggleClass($divs[$i], 'selected');
+				$(this).toggleClass('selected');
 			}
 		});
-	}
-};
+	});
 
-ready(main);
+});
