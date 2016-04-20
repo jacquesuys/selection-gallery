d29af428b351b374a68d03f4ab1b42a2fbe6355d
Jacques Uys <jacquesuys@Jacquess-MacBook-Air.local>
2016-04-20 12:17:19 +0200

    IE8 polyfills

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
-document.addEventListener("DOMContentLoaded", function(){
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+}
 
-	var $ = function(element) {
-		return document.querySelectorAll(element);
+var main = function(){
+
+	var $ = function(el) {
+		return document.querySelectorAll(el);
+	};
+
+	var addEventListener = function(el, eventName, handler) {
+	  if (el.addEventListener) {
+	    el.addEventListener(eventName, handler);
+	  } else {
+	    el.attachEvent('on' + eventName, function(){
+	      handler.call(el);
+	    });
+	  }
+	};
+
+	var toggleClass = function(el, className) {
+		if (el.classList) {
+		  el.classList.toggle(className);
+		} else {
+			var classes = el.className.split(' ');
+			var existingIndex = -1;
+			for (var i = classes.length; i--;) {
+				if (classes[i] === className) {
+					existingIndex = i;
+				}
+			}
+
+			if (existingIndex >= 0) {
+				classes.splice(existingIndex, 1);
+			} else {
+				classes.push(className);
+			}
+
+		  el.className = classes.join(' ');
+		}
 	};
 
 	var $menu = $('header > a')[0];
 	var $header = $('header')[0];
 
-	$menu.addEventListener('click', function(){
-		$header.classList.toggle('menu-open');
+	addEventListener($menu, 'click', function(){
+		toggleClass($header, 'menu-open');
 	});
 
 	var $divs = $('section > div');
@@ -22,24 +67,23 @@ document.addEventListener("DOMContentLoaded", function(){
 
 		$cover.style.backgroundImage = 'url(' + $img.src + ')';
 
-		$divs[i].addEventListener('click', function(){
+		addEventListener($divs[i], 'click', function(){
 			var $i = this.dataset.i;
 			var $indexOf = selected.indexOf($i);
 
 			if ( selected.length < 10 ) {
 				if ( $indexOf < 0 ) {
 					selected.push($i);
-					console.log(selected);
 				} else {
 					selected.splice($indexOf, 1);
-					console.log(selected);
 				}
-				$divs[$i].classList.toggle('selected');
+				toggleClass($divs[$i], 'selected');
 			} else if (selected.length === 10 && $indexOf > 0) {
 				selected.splice($indexOf, 1);
-				console.log(selected);
-				$divs[$i].classList.toggle('selected');
+				toggleClass($divs[$i], 'selected');
 			}
 		});
 	}
-});
+};
+
+ready(main);
